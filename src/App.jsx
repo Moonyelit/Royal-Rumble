ed4e1656a03c27a0d57d412866e7cef839e3b4fc
moonye@live.fr <moonye@live.fr>
2025-03-27 11:53:17 +0100

    ADD : home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 // App.jsx
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage"; // Chemin corrigé
 import BattleScene from "./components/BattleScene/BattleScene";
 
 function App() {
   return (
-    <div className="App">
-      <BattleScene />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/battle" element={<BattleScene />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './HomePage.css';
+
+function HomePage() {
+  const navigate = useNavigate();
+  
+  const handleStartCombat = () => {
+    // Son de clic éventuel
+    navigate('/battle');
+  };
+
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <img 
+          src="/public/Suikoden_II_Logo.svg.png" 
+          alt="Suikoden II Logo" 
+          className="home-logo"
+        />
+        <p className="home-description">
+          Affrontez Neclord avec Riou et ses compagnons dans ce combat épique!
+
+          <br /><br />
+          Le jeu a été pensé sur ordinateur. Il n'y a pas de version mobile de disponible pour le moment.
+        </p>
+        <button 
+          className="combat-button" 
+          onClick={handleStartCombat}
+        >
+          <i className="fas fa-sword mr-2"></i>
+          COMBATTRE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
