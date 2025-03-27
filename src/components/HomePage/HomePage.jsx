1e57bc41c64c7c383b33122f9750d37fe705bf2b
moonye@live.fr <moonye@live.fr>
2025-03-27 15:20:06 +0100

    Add modif

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './HomePage.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+import backgroundGif from "/BackgroundGif.gif"; // Import du GIF
 
 function HomePage() {
   const navigate = useNavigate();
-  
+
   const handleStartCombat = () => {
     // Son de clic éventuel
-    navigate('/battle');
+    navigate("/battle");
   };
 
   return (
-    <div className="home-container">
+    <div 
+      className="home-container" 
+      style={{ backgroundImage: `url(${backgroundGif})` }}
+    >
       <div className="home-content">
-        <img 
-          src="/public/Suikoden_II_Logo.svg.png" 
-          alt="Suikoden II Logo" 
-          className="home-logo"
-        />
-        <p className="home-description">
+      <img src="/Autre/Suikoden_II_Logo.svg.png" alt="Suikoden II Logo" className="home-logo" />
+      <p className="home-description">
           Affrontez Neclord avec Riou et ses compagnons dans ce combat épique!
-
-          <br /><br />
-          Le jeu a été pensé sur ordinateur. Il n'y a pas de version mobile de disponible pour le moment.
+          <br />
+          <br />
+          Le jeu a été pensé sur ordinateur. Il n'y a pas de version mobile de
+          disponible pour le moment.
         </p>
-        <button 
-          className="combat-button" 
-          onClick={handleStartCombat}
-        >
+        <button className="combat-button" onClick={handleStartCombat}>
           <i className="fas fa-sword mr-2"></i>
           COMBATTRE
         </button>
